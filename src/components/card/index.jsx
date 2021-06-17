755ce8138b3178d91f6a98533387d37caf2fea4f
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-06-17 08:02:40 +0700

    create card component

---
+++ src/components/card/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+const card = ({ headline, image }) => {
+    return (
+        <div>
+            <p>{headline}</p>
+        </div>
+    )
+}
+
+export default card
